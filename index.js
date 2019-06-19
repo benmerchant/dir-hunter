736ae46917726812d29864ad453677dee3086b91
benmerchant <benjaminmerchant@gmail.com>
2019-06-19 16:40:11 -0400

    add minor refactorings. add some TODOs

--- index.js
+++ index.js
@@ -12,6 +12,8 @@
  */
 'use strict';
 
+/** // TODO: create function that will accept the top dir and create it like the rest */
+
 const AppState = require('./lib/appstate.js');
 const Looper = require('./lib/looper');
 
@@ -19,4 +21,11 @@ const Looper = require('./lib/looper');
 const arrayFromFileFinderLibrary = Looper.loopDirentsArray();
 // 2. call the dir array looper
 Looper.oneIteration(arrayFromFileFinderLibrary,AppState);
-console.log(AppState);
+console.log('**********currentParent');
+/** // TODO: normalise TopDirentObj to have name like the rest of them */
+console.log(AppState.currentParent.name);
+AppState.currentParent.children.forEach((child) => {
+  console.log(child.name);
+})
+console.log('\n**********previousParent');
+console.log('TODO handle the first iteration where this is NULL');
