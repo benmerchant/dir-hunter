96d5e43d9751f3155f260e0e96baea212239534c
benmerchant <benjaminmerchant@gmail.com>
2019-06-21 12:52:56 -0400

    *FAILING* SoC: childArrayBuilder... have to cut grass...

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+'use strict';
 /**
  * @filename '/noitunes/index.js'
  * @fileoverview crawl through a directory and its 287 sub-directories
@@ -10,9 +11,6 @@
  *
  * @version 0.0.5 - restructure for recursion
  */
-'use strict';
-
-
 const DirectoryDE = require('./lib/dirents/directory.direntry');
 const FileDE = require('./lib/dirents/file.direntry');
 const AppConfig = require('./lib/appConfig.js');
@@ -21,29 +19,33 @@ const AppConfig = require('./lib/appConfig.js');
 /** // TODO: normalise TopDirentObj to have name like the rest of them */
 /** // TODO: change oneIteration fn so it does not alter state. and only has one return statement */
 
-
-
+/********* /MAIN/ *********/
 dirIterator();
+/********* /MAIN/ *********/
 
-function dirIterator(DirectoryEntryObject){
+function dirIterator(){
   /** // TODO: send files to FileDE()........ */
   let currentDirectory = new DirectoryDE(AppConfig._topDirObject);
   let previousDirectory = null;
 
-  const _dirIterator = (currentDirectory, previousDirectory) => {
-    if(currentDirectory.children.length > 0){
-        currentDirectory.children.forEach((child, ii, currDirChildArr) => {
-          dirIterator(child);
-        });
-        console.log(currentDirectory);
+  console.log(currentDirectory);
+let iii;
+  while(iii<1){
+    const _dirIterator = (currentDirectory, previousDirectory) => {
+      if(currentDirectory.children.length > 0){
+          currentDirectory.children.forEach((child, ii, currDirChildArr) => {
+            dirIterator(child);
+          });
+          console.log(currentDirectory);
+      }
     }
-  }
+
 
 
   _dirIterator(currentDirectory,previousDirectory);
-
+  iii += 1;
 }
-
+}
 function compareChildrenLenToDirentLen(currentParentChildLen, currentDirArrayLen) {
   // if children.length = direntArray.length, children have all been checked
   if(currentParentChildLen===currentDirArrayLen) return true;
